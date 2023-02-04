6c311a91ba2a45dd068cb89d8bdb6f31a654c52c
Abdullateef Ado <olatunbosunlateef6@gmail.com>
2023-02-04 07:44:45 -0800

    Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list[]):
+    if len(my_list) < 1 or my_list is None:
+        return None
+    maxi = my_list[0]
+    for no in my_list:
+        if no > maxi:
+            maxi = no 
+            return maxi
+
