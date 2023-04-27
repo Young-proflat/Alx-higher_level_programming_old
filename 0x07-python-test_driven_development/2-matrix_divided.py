22ea2ca4636a3e1f0ff05dbd21237b66e88b4346
root <olatunbosunlateef6@gmail.com>
2023-04-27 14:52:37 -0700

    Test_driven_development

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+""" It print out a matrix output divided 
+    by 2 in two decimal figure
+"""
+def matrix_divided(matrix, div):
+    
+
+    if not type(div) in (int, float):
+        raise TypeError('div must be a number')
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+    if not isinstance (matrix, list):
+        raise TypeError('Matrix must be a matrix must have the same size')
+
+    len_elem = 0
+    for elems in matrix:
+        if not elems or not isinstance (elems, list):
+            raise TypeError('Matrix must be a matrix (list of lists) of integers/floats')
+        if len_elem != 0 and len(elems) != len_elem:
+            raise TypeError('Each row of the matrix must have the same size')
+
+        for num in elems:
+            if not type(num) in (int, float):
+                raise TypeError('Matrix must be a matrix (list of lists) of integers/floats')
+        len_elems = len(elems)
+
+    m = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
+    return (m)
