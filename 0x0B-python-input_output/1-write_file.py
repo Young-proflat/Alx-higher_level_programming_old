7115b1f8dbdb67fdd46e7989f9f91b345ea01117
root <olatunbosunlateef6@gmail.com>
2023-05-19 07:50:52 -0700

    Python-Input_output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ Function that write a string text file and return the list of the string """
 
+
 def write_file(filename="", text=""):
     """ It returns the list of a string """
-    with open(filename "w", encoding="utf-8") as file1:
-        print(file1.write(text))
+    with open(filename "w" encoding="utf-8") as file1:
+        return file1.write(text)
