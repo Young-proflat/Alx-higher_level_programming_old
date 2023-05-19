fe16e41d101b807d69c87ad917eb6549e0fbc1af
root <olatunbosunlateef6@gmail.com>
2023-05-19 08:10:55 -0700

    Python-Input_output

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Write a string to a text file(UTF8) """
+
+
+def write_file(filename="", text=""):
+    """ It returns the list of a string """
+    with open(filename, "a", encoding="utf-8") as file1:
+        return file1.write(text)
