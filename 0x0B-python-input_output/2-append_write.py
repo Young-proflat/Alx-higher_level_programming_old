3a61fc58fd830d2763b462dbc07aace697b5f215
root <olatunbosunlateef6@gmail.com>
2023-05-19 08:56:48 -0700

    Python-Input_output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-""" Appending a string to a text file """
-
+"""Appends a string of a text file"""
 
 
 def append_write(filename="", text=""):
-    """ Append a string to a text file 
-    """ 
-    with open(filename, "a", encoding="utf-8") as file1:
-        return file1.write(text)
+    """Appends a string of a text file"""
+    with open(filename, 'a') as f:
+        return f.write(text)
