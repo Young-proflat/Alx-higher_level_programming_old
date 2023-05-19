de844dbfd6e816465f5894dd29ea45c98fd6e510
root <olatunbosunlateef6@gmail.com>
2023-05-19 08:53:38 -0700

    Python-Input_output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,6 +4,7 @@
 
 
 def append_write(filename="", text=""):
-    """ Append a string to a text file """ 
-    with open(filename, "a") as file1:
+    """ Append a string to a text file 
+    """ 
+    with open(filename, "a", encoding="utf-8") as file1:
         return file1.write(text)
