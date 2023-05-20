3fd4da0e6ec631d55b4719cbdca34a35227a201e
root <olatunbosunlateef6@gmail.com>
2023-05-20 07:22:38 -0700

    Python_Inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-""" Creating a class """
-
-
+"""Defines a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """A Square class that inherits from Rectangle"""
-    def __init__(self, size):
-        self.integer_validator("size", size)
-        self.__size = size
-        super().__init__(size, size)
+    """Represent a square"""
 
-    def area(self):
-        """ area method """
-        return self.__size ** 2
+    def __init__(self, size):
+        """Initialize a new square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
 """ A class that inherits from BaseGeometry """
-
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """ Rectangle class that inherits from BaseGeometry """
+    
     def __init__(self, width, height):
+        """Intialize a new Rectangle
+        """
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
-
-    def are(self):
-        """ area method """
-        return  self.__width * self.__height
-
-    def __str__(self):
-        """__str__ method"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
