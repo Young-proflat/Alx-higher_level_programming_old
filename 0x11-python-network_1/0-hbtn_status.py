163977199b25a9fb14364ddc84bd0b22e71fba44
Young-proflat <olatunbosunlateef6@gmail.com>
2023-07-26 23:14:32 +0000

    Pyhton -network_1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"This script fetches a urllib for this url"
+
+if "__name__" = "__main__":
+    import urllib.request
+
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as reponse:
+        html = reponse.read()
+
+        print("\tBody reponse: {}")
+        print("\ttype: {} " .format(type(html)))
+        print("\t -content: {}" .format (html))
+        print("\t -utf8 content: {}" .format(html.decode('utf8')))
