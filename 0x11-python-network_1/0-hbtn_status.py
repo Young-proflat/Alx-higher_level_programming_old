b077756110c40a787c01a78bc5d90de2c2797df3
Young-proflat <olatunbosunlateef6@gmail.com>
2023-07-26 23:32:50 +0000

    Python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 "This script fetches a urllib for this url"
 
-if "__name__" = "__main__":
+if "__name__" == "__main__":
     import urllib.request
 
     url = "https://alx-intranet.hbtn.io/status"
 
-    with urllib.request.urlopen(url) as reponse:
-        html = reponse.read()
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
 
-        print("\tBody reponse: {}")
-        print("\ttype: {} " .format(type(html)))
-        print("\t -content: {}" .format (html))
-        print("\t -utf8 content: {}" .format(html.decode('utf8')))
+        print("\tBody reponse:")
+        print("\ttype: {}" .format(type(html)))
+        print("\t -content: {}".format (html))
+        print("\t -utf8 content: {}".format(html.decode('utf8')))
