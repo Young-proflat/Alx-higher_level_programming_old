da0adaa36fcf9d17880be549ba10750c6c8ea7dc
root <olatunbosunlateef6@gmail.com>
2023-08-15 20:25:14 +0000

    Javascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const li = ['C is fun', 'Pyhton is cool','Javascript is amazing']
-for (let i = 0;i <= li.length; i++) {
+const li = ['C is fun', 'Pyhton is cool', 'Javascript is amazing'];
+for (let i = 0; i < li.length; i++) {
   console.log(li[i]);
-};
+}
