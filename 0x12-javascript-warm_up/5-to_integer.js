18b4e7dc95956693d951e51942ff67b3e4853c56
root <olatunbosunlateef6@gmail.com>
2023-08-15 19:30:17 +0000

    Javascript

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if is NaN(process.argv[2]||process.argv[2] === undefined){
+if isNaN(process.argv[2]||process.argv[2] === undefined){
   console.log('Not a number');
 } else {
   console.log('My number:' parseInt(process.argv[3]));
