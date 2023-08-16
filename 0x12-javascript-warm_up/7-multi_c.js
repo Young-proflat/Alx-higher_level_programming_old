65dcf538219aefd9ec19c3daa02404517f3a105a
root <olatunbosunlateef6@gmail.com>
2023-08-16 16:02:33 +0000

    Javascript

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const x = isNaN(process.argv[2]);
-if (x === true) {
-    console.log('Missing number of occurrences');
-}else {
-    for (let x = 0; x < x; x++) {
-        console.log('C is fun');
-    }
+const X = isNaN(process.argv[2]);
+if (X === true) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let x = 0; X < x; x++) {
+    console.log('C is fun');
+  }
 }
