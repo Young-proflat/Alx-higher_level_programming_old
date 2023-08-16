ad7007bd26101201ee7bcd080e98a4d1e2aa10c9
root <olatunbosunlateef6@gmail.com>
2023-08-16 00:11:45 +0000

    Javascript

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,8 +2,8 @@
 const x = isNaN(process.argv[2]);
 if (x === true){
     console.log('Missing number of occurrences');
-}else{
-    for (let x = 0 ; x < x; x++){
-        console.log('C is fun')
+}else {
+    for (let x = 0; x < x; x++) {
+        console.log('C is fun');
     }
 }
